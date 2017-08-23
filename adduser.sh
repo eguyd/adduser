0ac361b3f1e2dbff17c327a7a5123ee6247e0c18
Andrew Eckley <andrew.eckley@gmail.com>
2017-08-22 17:13:50 -0700

    Added server name to the script

--- adduser.sh
+++ adduser.sh
@@ -2,6 +2,8 @@
 # Stop on any errors
 set -e
 
+echo "Please enter the server's username:"
+read serveruser
 echo "Please enter the server's IP address that you want to add users to:"
 read address
 echo "Please enter the username"
@@ -14,7 +16,7 @@ read pubkey
 #Create authorized_keys file
 #Append public key to authorized_keys file
 #Adjust ownership and permissions of the .ssh folder and authorized_keys file
-ssh -t demo@$address 'sudo adduser --disabled-password --gecos "" ' $user '; sudo mkdir -p /home/'$user'/.ssh ; sudo touch /home/'$user'/.ssh/authorized_keys ;' "echo $pubkey | sudo tee /home/$user/.ssh/authorized_keys ; sudo chown -R $user:$user /home/$user/.ssh; sudo chmod 700 /home/$user/.ssh; sudo chmod 600 /home/$user/.ssh/authorized_keys"
+ssh -t $serveruser@$address 'sudo adduser --disabled-password --gecos "" ' $user '; sudo mkdir -p /home/'$user'/.ssh ; sudo touch /home/'$user'/.ssh/authorized_keys ;' "echo $pubkey | sudo tee /home/$user/.ssh/authorized_keys ; sudo chown -R $user:$user /home/$user/.ssh; sudo chmod 700 /home/$user/.ssh; sudo chmod 600 /home/$user/.ssh/authorized_keys"
 
 
 
